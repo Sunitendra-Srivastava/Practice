4b979a4ab545f2beb2db35e818e022df1d3acf03
sunitendra srivastava <sunitendras@gmail.com>
2025-03-12 16:12:39 +0530

    JS intro

---
+++ webtech/Js1/script.js
@@ -0,0 +1,13 @@
+alert("hello World!")
+console.log("The code is running....")
+
+var a  = prompt("Enter your number")
+console.log("Your number is " +a)
+
+var isTrue = confirm("Are you sure you want to blast your computer.")
+if(isTrue){
+    console.log("Computer is blasting.")
+}
+else{
+    console.log("Computer is not blasting.")
+}
